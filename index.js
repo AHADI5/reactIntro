437c4c0012327c2108cd2757de0b5c739c42e5ac
AHADI5 <gloireahadi9@gmail.com>
2024-02-03 08:31:08 +0100

    components

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const container = document.getElementById("root") ;
-const page = (
-  <div >
-    <img src = "./reactLogo.png"/>
-    <h1>Fun Facts About React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of entreprise apps , including mobile apps </li>
-    </ul>
-  </div>
- 
-)
 
-ReactDOM.render(page , container)
+
+function Header () {
+  return (
+    <nav>
+      <img src = "./reactLogo.png"/>
+    </nav>
+  )
+}
+
+function Description () {
+  return (
+    <h1>Reasons I'm excited to learn React</h1>
+  )
+}
+
+function Footer() {
+  return (
+    <p>&copy 20xx A-2G development. All rights reserved</p>
+  )
+}
+function Reasons () {
+  return (
+    <ol>
+      <li>It's pretty librabry</li>
+      <li>It's Easier to learn</li>
+    </ol>
+  )
+}
+
+ReactDOM.render(
+
+<div>
+  <Header/>
+  <Reasons/>
+  <Footer/>
+</div>
+,  document.getElementById("root") )
